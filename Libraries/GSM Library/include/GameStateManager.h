3a3bfed80318e3ad40976982908b2b61e3d17bfa
DarthDementous <sebtoy@live.com>
2017-08-13 20:38:02 +1000

    - Updated libraries with ones in local Bootstrap solution.

--- Libraries/GSM Library/include/GameStateManager.h
+++ Libraries/GSM Library/include/GameStateManager.h
@@ -4,27 +4,53 @@
 #include <Stack.hpp>
 #include <Queue.hpp>
 #include <Map.hpp>
+#include <vector>
+
+namespace aie {
+	class Renderer2D;
+}
 
 class GameStateManager {
 public:
 	/*
 	*	@brief	Class that handles which states are active.
+	*	@param	a_r2d is the pointer to renderer2D used to render states.
 	*/
 	GameStateManager();
 	~GameStateManager();
 
 	void Update(float deltaTime);
-	void Draw();
+	void Draw(aie::Renderer2D* a_r2d);
 
 	void PushState(const char* name, IGameState *state);
 	void SetState(const char* name);
+	void SetState(IGameState* state);
 	void PopState();
 
 	/**
-	*	@brief Get top state off the stack.
-	*	@return State at the end of the container.
+	*	@brief Get specified state from available states by key.
+	*	@param a_name is the name of the state.
+	*	@return desired state.
 	*/
-	IGameState* GetTopState() { return m_activeStates.Top(); }
+	IGameState* GetState(const char* a_name);
+
+	/**
+	*	@brief Return all current active states.
+	*	@return Vector of active states (empty if no states active)
+	*/
+	std::vector<IGameState*> GetActiveStates();
+
+	/**
+	*	@brief Return all available states.
+	*	@return Vector of possible states (empty if no possible states)
+	*/
+	std::vector<IGameState*> GetStates();
+
+	/**
+	*	@brief Get top state off the stack.
+	*	@return State at the end of the container, if no active states then returns nullptr.
+	*/
+	IGameState* GetTopState();
 
 	/**
 	*	@brief Send command to pause current states
@@ -39,7 +65,8 @@ public:
 private:
 	// Commands to be queued and executed
 	enum class eCommands {
-		SET,
+		SET_BYNAME,
+		SET_BYSTATE,
 		PUSH,
 		POP,
 		PAUSE,
@@ -71,10 +98,16 @@ private:
 	void DoPushState(const char* name, IGameState *state);
 
 	/**
-	*	@brief	Add the active state by key to the active state vector
+	*	@brief	Add the active state by key to the active state stack.
 	*	@param	name is the key that identifies the desired state.
 	*/
 	void DoSetState(const char* name);
+
+	/**
+	*	@brief Add the active state by value to the active state stack.
+	*	@param state is the pointer to the state to go onto the active stack (must already be in available states)
+	*/
+	void DoSetState(IGameState* state);
 
 	/**
 	*	@brief	Removes a state from the end of the active state container.
--- Libraries/GSM Library/include/IGameState.hpp
+++ Libraries/GSM Library/include/IGameState.hpp
@@ -12,15 +12,15 @@ public:
 	*	@brief	Base class that defines states of operation for an application.
 	*	@param	a_r2d is the renderer pointer that we hold onto to draw states.
 	*/
-	IGameState(aie::Renderer2D* a_r2d = nullptr) : m_r2d(a_r2d) {
+	IGameState() {
 		Initialise();
 	}
 	virtual ~IGameState() {}						// Base classes that are inherited from must have virtual destructor so memory clean up happens for base class as well
 
-	virtual void Initialise() = 0;
+	virtual void Initialise() {}
 	virtual void Startup() = 0;
 	virtual void Update(float deltaTime) = 0;
-	virtual void Draw() = 0;
+	virtual void Draw(aie::Renderer2D* a_r2d) = 0;
 	virtual void Shutdown() = 0;
 
 	void SetUpdateActive(bool a_val) { updateIsActive = a_val; }
@@ -30,8 +30,6 @@ public:
 	bool IsDrawActive() { return drawIsActive; }
 
 protected:
-	aie::Renderer2D* m_r2d = nullptr;
-
 	bool updateIsActive = true;
 	bool drawIsActive = true;
 private:
